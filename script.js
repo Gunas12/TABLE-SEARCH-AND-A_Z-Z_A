353de2159431299a37ad78bb928f76a95eac1eb7
Gunas12 <104676163+Gunas12@users.noreply.github.com>
2022-12-13 09:16:25 +0400

    with search

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let tbody = document.querySelector("tbody")
-let a_z=document.querySelector("#sort_down")
-let z_a=document.querySelector("#sort_up")
-let a_z1=document.querySelector("#sort_down1")
-let z_a1=document.querySelector("#sort_up1")
+let a_z = document.querySelector("#sort_down")
+let z_a = document.querySelector("#sort_up")
+let a_z1 = document.querySelector("#sort_down1")
+let z_a1 = document.querySelector("#sort_up1")
 
 let arr = [];
 
@@ -10,11 +10,11 @@ let arr = [];
 function createtr(element) {
 
   let tr = document.createElement("tr")
-  let name= document.createElement("th")
+  let name = document.createElement("th")
   let unitprice = document.createElement("td")
   let unitinstock = document.createElement("td")
-  let quantity= document.createElement("td")
-tbody.appendChild(tr)
+  let quantity = document.createElement("td")
+  tbody.appendChild(tr)
   tr.appendChild(name)
   tr.appendChild(unitprice)
   tr.appendChild(unitinstock)
@@ -25,7 +25,7 @@ tbody.appendChild(tr)
   unitprice.innerText = element.unitPrice
   unitinstock.innerText = element.unitsInStock
   quantity.innerText = element.quantityPerUnit
-  
+
 }
 
 
@@ -33,95 +33,94 @@ axios.get("https://northwind.vercel.app/api/products")
   .then(res => res.data)
   .then(data => data.forEach(element => {
     createtr(element);
+    arr.push(element)
+
   }))
-  .then(res => {
-    arr = res.data;
-    console.log(arr);
-  })
 
 
 
-z_a.addEventListener("click",function(){
-    tbody.innerText=" "
-    console.log(tbody);
-    axios.get("https://northwind.vercel.app/api/products")
-    .then(res=>{
-        let newArr1;
-        newArr1=res.data
-         res.data.sort((a,b)=>b.name.localeCompare(a.name)).forEach(element => {
-            createtr(element);
-          }
-          )
-        
+
+z_a.addEventListener("click", function () {
+  tbody.innerText = " "
+  console.log(tbody);
+  axios.get("https://northwind.vercel.app/api/products")
+    .then(res => {
+      let newArr1;
+      newArr1 = res.data
+      res.data.sort((a, b) => b.name.localeCompare(a.name)).forEach(element => {
+        createtr(element);
+      }
+      )
+
     })
 
-        .catch((err)=> console.log(err))
-    
-  })
+    .catch((err) => console.log(err))
 
-a_z.addEventListener("click",function(){
-    tbody.innerText=" "
-    
-    axios.get("https://northwind.vercel.app/api/products")
-    .then(res=>{
-        let newArr2;
-        newArr2=res.data
-         res.data.sort((a,b)=>a.name.localeCompare(b.name)).forEach(element => {
-            createtr(element);
-          }
-          ) 
+})
+
+a_z.addEventListener("click", function () {
+  tbody.innerText = " "
+
+  axios.get("https://northwind.vercel.app/api/products")
+    .then(res => {
+      let newArr2;
+      newArr2 = res.data
+      res.data.sort((a, b) => a.name.localeCompare(b.name)).forEach(element => {
+        createtr(element);
+      }
+      )
     })
-       .catch((err)=> console.log(err))
-    
-  })
+    .catch((err) => console.log(err))
+
+})
 
 
-z_a1.addEventListener("click",function(){
-    tbody.innerText=" "
-    console.log(tbody);
-    axios.get("https://northwind.vercel.app/api/products")
-    .then(res=>{
-        let newArr1;
-        newArr1=res.data
-         res.data.sort((a, b) => b.unitPrice - a.unitPrice).forEach(element => {
-            createtr(element);
-          }
-          )
-        
+z_a1.addEventListener("click", function () {
+  tbody.innerText = " "
+  console.log(tbody);
+  axios.get("https://northwind.vercel.app/api/products")
+    .then(res => {
+      let newArr1;
+      newArr1 = res.data
+      res.data.sort((a, b) => b.unitPrice - a.unitPrice).forEach(element => {
+        createtr(element);
+      }
+      )
+
     })
 
-        .catch((err)=> console.log(err))
-    
-  })
+    .catch((err) => console.log(err))
 
-a_z1.addEventListener("click",function(){
-    tbody.innerText=" "
-    
-    axios.get("https://northwind.vercel.app/api/products")
-    .then(res=>{
-        let newArr2;
-        newArr2=res.data
-         res.data.sort((a, b) => a.unitPrice - b.unitPrice).forEach(element => {
-            createtr(element);
-          }
-          ) 
+})
+
+a_z1.addEventListener("click", function () {
+  tbody.innerText = " "
+
+  axios.get("https://northwind.vercel.app/api/products")
+    .then(res => {
+      let newArr2;
+      newArr2 = res.data
+      res.data.sort((a, b) => a.unitPrice - b.unitPrice).forEach(element => {
+        createtr(element);
+      }
+      )
     })
-       .catch((err)=> console.log(err))
-    
-  })
+    .catch((err) => console.log(err))
 
-// let tabletr=document.querySelectorAll("td")
-  let input=document.querySelector("#search")
-input.addEventListener("input",function(){
-    let newArr = res;
-    console.log(newArr)
-});
+})
+
+let input = document.querySelector("#search")
+
+input.addEventListener("input", function () {
+  tbody.innerText = " "
+  arr.filter(element =>
+    element.name.toLowerCase().includes(input.value.toLowerCase()))
+    .forEach(element => {
+      createtr(element)
+    })
+
+})
 
 
 
-  
 
-  
-
-
- 
